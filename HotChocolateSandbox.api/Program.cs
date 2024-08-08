3d4430707d819049137dca99dbe59fb1d1fa6cdc
Kevin Wüstner <kevin.wuestner@xappido.com>
2024-08-08 15:18:36 +0200

    feat(data): init db

--- HotChocolateSandbox.api/Program.cs
+++ HotChocolateSandbox.api/Program.cs
@@ -1,4 +1,10 @@
+using HotChocolateSandbox.api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContextPool<HotChocolateSandboxDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("CatalogDB")));
 
 builder.Services
     .AddGraphQLServer()
