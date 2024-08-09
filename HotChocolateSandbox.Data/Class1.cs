88d31d74006d04764fe0fdbb540483856f82c80b
Kevin Wüstner <kevin.wuestner@xappido.com>
2024-08-09 10:25:53 +0200

    feat(data.model): add models

--- HotChocolateSandbox.Data/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace HotChocolateSandbox.Data;
-
-public class Class1
-{
-}
---
+++ HotChocolateSandbox.Data/Model/Book.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HotChocolateSandbox.api.Model;
+
+public class Book
+{
+    [Key]
+    public Guid Id { get; init; }
+    public string Title { get; private set; }
+    public Author Author { get; private set; }
+    public Guid AuthorId { get; private set; }
+
+    private Book()
+    {
+    }
+
+    private Book(string title, Author author)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        Author = author;
+    }
+
+    public static Book Create(string title, Author author)
+    {
+        return new Book(title, author);
+    }
+}
