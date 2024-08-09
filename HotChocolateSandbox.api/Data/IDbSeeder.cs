9c18b9e8bddb67a3ff478ec958fbf6a7e6d7eaa0
Kevin Wüstner <kevin.wuestner@xappido.com>
2024-08-09 10:52:21 +0200

    feat(pool): delete api project

--- HotChocolateSandbox.api/Data/IDbSeeder.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace HotChocolateSandbox.api.Data;
-
-public interface IDbSeeder<in TContext> where TContext : DbContext
-{
-    Task SeedAsync(TContext context);
-}
