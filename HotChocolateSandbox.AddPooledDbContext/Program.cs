06adb19cc9b7f7e99445c1e58f2a3b5a042c803a
Kevin Wüstner <kevin.wuestner@xappido.com>
2024-08-09 10:38:58 +0200

    feat(pool): add services to program.cs

--- HotChocolateSandbox.AddPooledDbContext/Program.cs
+++ HotChocolateSandbox.AddPooledDbContext/Program.cs
@@ -1,4 +1,17 @@
+using HotChocolateSandbox.api.Data;
+using HotChocolateSandbox.api.Service;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContextPool<HotChocolateSandboxDbContext>(o
+    => o.UseNpgsql(builder.Configuration.GetConnectionString("CatalogDB")));
+
+builder.Services.AddMigration<HotChocolateSandboxDbContext, HotChocolateSandboxSeed>();
+
+builder.Services
+    .AddScoped<BookService>()
+    .AddScoped<AuthorService>();
 
 builder.Services
     .AddGraphQLServer()
